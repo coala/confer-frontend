dd3f56891fac36e9815ddab67cba9fe6f61692aa
dylansavoia@gmail.com <dylansavoia@gmail.com>
2017-04-06 20:46:16 +0200

    Get Data from API

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 var app = angular.module('conference', [])
-app.controller('searchCtrl', ['$scope', function($scope){
+app.controller('searchCtrl', ['$scope', "$http", function($scope, $http){
   $scope.conferences = []
 
-  var temp_conf_object = {
-    "name" : "Conf1",
-    "desc" : `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                 tempor incididunt ut labore et dolore magna aliqua.`,
-    "venue" : "Here",
-    "time" : "9:30",
-    "image" : "http://placehold.it/250x200"
-  }
 
-  for (var i = 0; i < 9; i++) {
-        $scope.conferences.push(temp_conf_object);
-  }
+    var url = "http://conferback.coala.io/conferences/api/conferences/";
+    
+    $http({
+        method: "GET",
+        url: url
+    }).success(function(res){
+        for (var i = 0; i < res.length; i++){
+            $scope.conferences.push(temp_conf_object);
+        }
+    });
+
+  
 
 }])
